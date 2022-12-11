454eb016990dbd6049f537cbb4648279b1bd9f66
mjjauhar <mjjauharali@gmail.com>
2022-12-11 15:15:50 +0530

    changed connection to app.js

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 // npm modules
+const mongoose = require("mongoose");
 const express = require("express");
-const session = require("express-session");
 const app = express();
+const session = require("express-session");
+const mongoDBSession = require("connect-mongodb-session")(session);
+require("dotenv").config();
 const ejs = require("ejs");
 const multer = require("multer");
 
 // custom imports
-const connection = require("./config/connection");
 const userRouter = require("./routes/user.routes");
 const adminRouter = require("./routes/admin.routes");
+
+const mongoURI = process.env.ATLAS_DB_LINK;
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((res) => {
+    console.log("MongoDB connected");
+    app.listen(7000, () =>
+      console.log("Server running on port http://localhost:7000/")
+    );
+  })
+  .catch((err) => console.log(err));
+
+const store = new mongoDBSession({
+  uri: mongoURI,
+  collection: "mySession",
+});
 
 // session configurations
 app.use(function (req, res, next) {
@@ -20,9 +38,15 @@ app.use(
     secret: "mykey",
     resave: false,
     saveUninitialized: false,
-    store: connection.store,
+    store: store,
   })
 );
+
+// Get the default connection
+const db = mongoose.connection;
+
+// Bind connection to error event (to get notification of connection errors)
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 // photo and other file upload using multer
 const fileStorage = multer.diskStorage({
@@ -35,7 +59,7 @@ const fileStorage = multer.diskStorage({
   },
 });
 
-app.use(multer({ storage: fileStorage }).array("imgUrl",5));
+app.use(multer({ storage: fileStorage }).array("imgUrl", 5));
 
 // static folder
 app.use("/public", express.static("public"));
@@ -46,10 +70,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/admin", adminRouter);
 app.use("/", userRouter);
 
-app.use("*",(req,res)=>{
-  res.render('404/404');
-})
+app.use("*", (req, res) => {
+  res.render("404/404");
+});
 
 // app.listen(8000, () =>
 //   console.log("Server running on port http://localhost:8000/")
 // );
+
+// Set up default mongoose connection
+// const mongoURI = "mongodb://localhost:27017/travel";
+// Mongo DB conncetion
